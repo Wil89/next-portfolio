ba3513b3a6e6c0b413c44f7b468ce23aee3fa219
Wil89 <wilber@fonoma.com>
2022-09-18 13:04:19 +0200

    experience section

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,15 +1,27 @@
 import React from "react";
+import ExperienceCard from "./ExperienceCard";
+const { motion } = require("framer-motion");
 
 type Props = {};
 
 const WorkExperience = (props: Props) => {
   return (
-    <div className="h-screen flex flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center relative overflow-hidden">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen flex flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center relative overflow-hidden"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
-      
-    </div>
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
+    </motion.div>
   );
 };
 
