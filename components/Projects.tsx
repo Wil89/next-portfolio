956c089d8e02c72b448fbe62b7ca857b578c90bd
Wil89 <wilber@fonoma.com>
2022-09-24 14:18:51 +0200

    project section

---
+++ components/Projects.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+const { motion } = require("framer-motion");
+
+type Props = {};
+
+const Projects = (props: Props) => {
+  const projects = [1, 2, 3, 4, 5, 6];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row
+  max-w-full justify-evenly mx-auto items-center z-0"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {/* projects */}
+        {projects.map((project, i) => (
+          <div
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5
+          items-center justify-center p-20 md:p-44 h-screen"
+          >
+            <motion.img
+              initial={{ y: -300, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.2 }}
+              src="/images/fonoma-desktop.png"
+              alt=""
+            />
+            <div className="space-y-10 px-0 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#f7ab0a]/50">
+                  Project {i + 1} of {projects.length} Fonoma LLC:
+                </span>{" "}
+                Fullstack dev
+              </h4>
+              <p className="text-lg text-center md:text-left">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px] -skew-y-12"></div>
+    </motion.div>
+  );
+};
+
+export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -28,6 +29,9 @@ const Home: NextPage = () => {
       </section>
       <section id="skills" className="snap-center">
         <Skills />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects />
       </section>
     </div>
   );
