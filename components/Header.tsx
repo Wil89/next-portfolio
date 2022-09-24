d3cefa3845b5991f6e903d74d70c2ebd5230f38b
Wil89 <wilber@fonoma.com>
2022-09-24 19:38:39 +0200

    scrollbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 const { motion } = require("framer-motion");
+import Link from "next/link";
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 
@@ -9,8 +10,8 @@ export const Header = (props: Props) => {
     <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
         initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate ={{x: 0, opacity: 1, scale: 1}}
-        transition={{duration: 0.5 ,}}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
         className="flex flex-row items-center"
       >
         <SocialIcon
@@ -29,21 +30,24 @@ export const Header = (props: Props) => {
           bgColor="transparent"
         />
       </motion.div>
-      <motion.div
-      initial={{x:500, opacity: 0, scale: 0.5}}
-      animate = {{x: 0, opacity: 1, scale: 1}} 
-      transition = {{duration: 0.5}}
-      className="flex flex-row items-center text-gray-300 cursor-pointer">
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Get in touch
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{ x: 500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get in touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 };
